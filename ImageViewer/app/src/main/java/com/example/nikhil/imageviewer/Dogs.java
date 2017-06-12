4c00f463a93dca99360a3ed7c956a2c84acdf485
Nikhil Londhe <Nikhil Londhe>
2017-06-12 19:09:18 -0400

    min Target OS: V4.4 API 19

---
+++ ImageViewer/app/src/main/java/com/example/nikhil/imageviewer/Dogs.java
@@ -0,0 +1,14 @@
+package com.example.nikhil.imageviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dogs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_blank);
+
+    }
+}
