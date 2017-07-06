a8decd67870a1f3bb32abfc26f7883d7b31f6024
nikhillondhe9 <nikhillondhe9@gmail.com>
2017-07-06 00:23:59 -0400

    Min SDK API 19

---
+++ CurvedMotionExercise/app/src/main/java/com/example/nikhil/curvedmotionexercise/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.nikhil.curvedmotionexercise;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private SceneAnimator sceneAnimator = null;
+    private FrameLayout container;
+    private Context themedContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        container = (FrameLayout) findViewById(R.id.container);
+
+        setupToolbar();
+        if (!setupModeSelection()) {
+        }
+    }
+
+   private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.app_name);
+            themedContext = actionBar.getThemedContext();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setLollipopAnimator() {
+        if (!(sceneAnimator instanceof LollipopSceneAnimator)) {
+            container.removeAllViews();
+            sceneAnimator = LollipopSceneAnimator.newInstance(this, container, R.layout.scene1, R.layout.scene2, R.transition.arc1);
+        }
+    }
+
+
+    private boolean setupModeSelection() {
+        Spinner spinner = (Spinner) findViewById(R.id.mode);
+        if (spinner == null || Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return false;
+        }
+        String[] values = getResources().getStringArray(R.array.modes);
+        SpinnerAdapter adapter = new ArrayAdapter<>(themedContext, android.R.layout.simple_spinner_dropdown_item, values);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        spinner.setSelection(0);
+        return true;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (position == 0) {
+            setLollipopAnimator();
+        } else {
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
---
+++ CurvedMotionExercise/app/src/main/java/com/example/nikhil/curvedmotionexercise/SceneAnimator.java
@@ -0,0 +1,8 @@
+package com.example.nikhil.curvedmotionexercise;
+
+/**
+ * Created by Nikhil on 7/5/2017.
+ */
+
+interface SceneAnimator {
+}
